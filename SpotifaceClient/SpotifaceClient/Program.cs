0c0d01ef8be16f6aeb1961e5cfe752963ad8654c
Rose <rosehirigoyen@gmail.com>
2020-04-30 00:16:04 -0400

    kinda working, some sync issue maybe?

--- SpotifaceClient/SpotifaceClient/Program.cs
+++ SpotifaceClient/SpotifaceClient/Program.cs
@@ -4,10 +4,10 @@ using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
 using SpotifyAPI.Web.Models;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using ZeroMQ;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace SpotifyAPI.Web.Examples.CLI
 {
@@ -15,17 +15,20 @@ namespace SpotifyAPI.Web.Examples.CLI
     {
         private static string _clientId = ""; //"";
         private static string _secretId = ""; //"";
+        private  const int CHANGE_SONG_BUFFER_MS = 1000;
 
-        private static Dictionary<string,string> _playlistMap = new Dictionary<string, string>()
+        private static SpotifyWebAPI _spotify;
+        private static readonly Dictionary<string,string> _playlistMap = new Dictionary<string, string>()
         {
-            {"happy","70x6C39NKrjYZryhRkuxfJ"},
-            {"sad", "5Ty5Wicbp9UOjBxdylJ3yA"},
-            {"neutral", "4bXVAmbOjChI697fuBMPLY"},
-            {"angry", "2H7ZU6MppCCjc0Qm3arYOy"},
-            {"disgust", "3KpNiPpOXiq7hr5vjw57mg"},
-            {"fear", "41LWUS2eOZeyjd0aWK55mX"},
-            {"surprise", "3wSHHOPz9NXzfsfAjq3RL8"}
+            {"happy",       "spotify:playlist:70x6C39NKrjYZryhRkuxfJ"},
+            {"sad",         "spotify:playlist:5Ty5Wicbp9UOjBxdylJ3yA"},
+            {"neutral",     "spotify:playlist:4bXVAmbOjChI697fuBMPLY"},
+            {"angry",       "spotify:playlist:2H7ZU6MppCCjc0Qm3arYOy"},
+            {"disgust",     "spotify:playlist:3KpNiPpOXiq7hr5vjw57mg"},
+            {"fear",        "spotify:playlist:41LWUS2eOZeyjd0aWK55mX"},
+            {"surprise",    "spotify:playlist:3wSHHOPz9NXzfsfAjq3RL8"}
         };
+
         // ReSharper disable once UnusedParameter.Local
         public static void Main(string[] args)
         {
@@ -36,10 +39,10 @@ namespace SpotifyAPI.Web.Examples.CLI
             _secretId = string.IsNullOrEmpty(_secretId) ?
               Environment.GetEnvironmentVariable("SPOTIFACE_SECRET") :
               _secretId;
-                
+
             var auth =
               new AuthorizationCodeAuth(_clientId, _secretId, "http://localhost:4002", "http://localhost:4002",
-                Scope.PlaylistReadPrivate | Scope.PlaylistReadCollaborative);
+                Scope.PlaylistReadPrivate | Scope.PlaylistReadCollaborative | Scope.UserReadPlaybackState | Scope.UserModifyPlaybackState);
             auth.AuthReceived += AuthOnAuthReceived;
             auth.Start();
             auth.OpenBrowser();
@@ -47,24 +50,38 @@ namespace SpotifyAPI.Web.Examples.CLI
             using (var context = new ZContext())
             using (var socket = new ZSocket(context, ZSocketType.REP))
             {
-                socket.Bind("tcp://127.0.0.1:5555");
+                var emotionStack = new List<string>();
+                var lastMostCommon = "";
 
-                while(true)
+                socket.Bind("tcp://127.0.0.1:5555");
+                LaunchEmotionDetection(Path.GetFullPath(@"..\..\..\..\..\src\") , "video_emotion_color_demo.py");
+
+                while (true)
                 {
-                    Console.Write("waiting");
                     using (ZFrame reply = socket.ReceiveFrame())
                     {
-                        String msg = reply.ReadString();
-                        Console.WriteLine("Hello {0}!", msg);
+                        emotionStack.Add(reply.ReadString());
+                        Console.WriteLine("RECIEVED {0}", emotionStack[emotionStack.Count - 1]);
                     }
-
                     socket.Send(new ZFrame("Thanks"));
 
+                    PlaybackContext playbackContext = _spotify.GetPlayback();
+                    var isReadyForNewSong = !playbackContext.IsPlaying || playbackContext.ProgressMs >= (playbackContext.Item.DurationMs - CHANGE_SONG_BUFFER_MS);
+                    Console.WriteLine("{0} ms before next song!", playbackContext.Item.DurationMs - CHANGE_SONG_BUFFER_MS - playbackContext.ProgressMs);
+                    if (isReadyForNewSong) {
+                        Console.WriteLine("READY FOR A NEW SONG");
+                        string mostCommon = emotionStack.GroupBy(v => v).OrderByDescending(g => g.Count()).First().Key;
+                        if (mostCommon != lastMostCommon)
+                        {
+                            Console.WriteLine("Most common since last time was {0}", mostCommon);
+                            lastMostCommon = mostCommon;
+                            _spotify.ResumePlayback(contextUri: _playlistMap[mostCommon], offset: 0, positionMs: 0);
+                            _spotify.SetShuffle(true);
+                            emotionStack.Clear();
+                        }
+                    }
                 }
             }
-
-            auth.Stop(0);
-
         }
 
         private static async void AuthOnAuthReceived(object sender, AuthorizationCode payload)
@@ -73,12 +90,13 @@ namespace SpotifyAPI.Web.Examples.CLI
             auth.Stop();
 
             Token token = await auth.ExchangeCode(payload.Code);
-            var api = new SpotifyWebAPI
+            _spotify = new SpotifyWebAPI
             {
                 AccessToken = token.AccessToken,
                 TokenType = token.TokenType
             };
-            await PrintUsefulData(api);
+
+            await PrintUsefulData(_spotify);
         }
 
         private static async Task PrintAllPlaylistTracks(SpotifyWebAPI api, Paging<SimplePlaylist> playlists)
@@ -100,11 +118,17 @@ namespace SpotifyAPI.Web.Examples.CLI
             await PrintAllPlaylistTracks(api, api.GetUserPlaylists(profile.Id));
         }
 
-        private static List<SpotifyAPI.Web.Models.PlaylistTrack> GetPlaylistSongs(SpotifyWebAPI api, String playlistID)
+        private static void LaunchEmotionDetection(string cwd, string cmd)
         {
-            Paging<PlaylistTrack> playlist = api.GetPlaylistTracks(playlistID);
-            playlist.Items.ForEach(track => Console.WriteLine(track.Track.Name));
-            return playlist.Items;
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                FileName = @"C:\Users\Rose Hirigoyen\AppData\Local\Programs\Python\Python37\python.exe",
+                Arguments = string.Format("{0}", cmd),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                WorkingDirectory = cwd
+            };
+            var proc = Process.Start(start);
         }
     }
 }
